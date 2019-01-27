c18f7aa2ed6467c62d7551841ae386f1b320126a
mspenski <44811974+mspenski@users.noreply.github.com>
2019-01-27 16:57:55 -0500

    Restructured the values to make code more DRY

--- bamazonDB.sql
+++ bamazonDB.sql
@@ -19,33 +19,15 @@ CREATE TABLE products (
 );
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Toothbrush", "Home", 5.00, 200);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("TV", "Electronics", 450.00, 30);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Jersey", "Clothing", 120.00, 60);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Phone", "Electronics", 700.00, 100);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Xbox", "Electronics", 350.00, 100);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Speaker", "Electronics", 60.00, 150);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Shoes", "Clothing", 90.00, 75);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Desk", "Home", 160.00, 20);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Tablet", "Electronics", 500.00, 80);
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Lamp", "Home", 35.00, 60);
+VALUES ("Toothbrush", "Home", 5.00, 200),
+ ("TV", "Electronics", 450.00, 30),
+ ("Jersey", "Clothing", 120.00, 60),
+ ("Phone", "Electronics", 700.00, 100),
+ ("Xbox", "Electronics", 350.00, 100),
+ ("Speaker", "Electronics", 60.00, 150),
+ ("Shoes", "Clothing", 90.00, 75),
+ ("Desk", "Home", 160.00, 20),
+ ("Tablet", "Electronics", 500.00, 80),
+ ("Lamp", "Home", 35.00, 60);
 
 SELECT * FROM products;
