8ab3aba5a2ace26e5636719bed198a5342697ef9
mspenski <44811974+mspenski@users.noreply.github.com>
2019-01-30 18:22:02 -0500

    finished with part two

--- bamazonManager.js
+++ bamazonManager.js
@@ -29,7 +29,7 @@ function showMenu() {
         name: "managerMenu",
         type: "list",
         message: "Welcome to the Main Menu!",
-        choices: ["View Products", "Low Inventory", "Add To Inventory", "Add New Product"]
+        choices: ["View Products", "Low Inventory", "Add To Inventory", "Add New Product", "Exit"]
       }
     ])
     .then(function (answer) {
@@ -41,6 +41,8 @@ function showMenu() {
         addInventory();
       } else if (answer.managerMenu === "Add New Product") {
         addProduct();
+      } else if (answer.managerMenu === "Exit") {
+        exit();
       }
     })
 }
@@ -51,29 +53,138 @@ function viewProducts() {
   connection.query("SELECT item_id, product_name, price, stock_quantity FROM products", function (err, res) {
     if (err) throw err;
     console.log(res)
+    showMenu()
   })
-  connection.end();
 };
 
 // function for showing items with a count of <5
 function lowInventory() {
-  console.log("products with low inventory");
+
   connection.query("SELECT product_name, stock_quantity FROM products WHERE stock_quantity < 6", function (err, res) {
-    // if (err) throw err;
-    // console.log(res);
-    if (res === []) {
-      console.log("No products currently low on inventory")
-    } else { console.log("deez nuts") }
+    if (err) throw err;
+
+    if (!res.length) {
+      console.log("\nNO PRODUCTS CURRENTLY LOW ON INVENTORY\n");
+      showMenu();
+    } else {
+      console.log("products with low inventory:\n");
+      console.log(res);
+      console.log("\n")
+      showMenu();
+    }
   })
-  connection.end();
+
 };
 
 // your app should display a prompt that will let the manager "add more" of any item currently in the store.
 function addInventory() {
   console.log("add inventory");
-};
+  connection.query("SELECT * FROM products", function (err, res) {
+    if (err) throw err;
+
+    inquirer
+      .prompt([
+        {
+          name: "chooseProduct",
+          type: "input",
+          message: "What is the ID of the product that you want to add inventory to?",
+          validate: function (value) {
+            if (isNaN(value) === false) {
+              return true;
+            }
+            return false;
+          }
+        },
+        {
+          name: "howMany",
+          type: "input",
+          message: "How many would you like to add?",
+          validate: function (value) {
+            if (isNaN(value) === false) {
+              return true;
+            }
+            return false;
+          }
+        }
+      ])
+
+      .then(function (answer) {
+        // console.log(answer)
+        // console.log(res)
+        var chosenItem;
+        var newInventory;
+        for (var i = 0; i < res.length; i++) {
+          if (parseInt(res[i].item_id) === parseInt(answer.chooseProduct)) {
+            chosenItem = res[i].item_id;
+            var newInventory = parseInt(answer.howMany) + parseInt(res[i].stock_quantity);
+            connection.query("UPDATE products SET ? WHERE ?",
+              [{
+                stock_quantity: newInventory
+              },
+              {
+                item_id: chosenItem
+              }]);
+            console.log("Item added successfully!")
+            showMenu();
+          }
+        }
+      });
+  });
+}
 
 // allow the manager to add a completely new product to the store.
 function addProduct() {
   console.log("add product");
+  inquirer
+    .prompt([
+      {
+        name: "itemName",
+        type: "input",
+        message: "What is the name of the item you would like to add?"
+      },
+      {
+        name: "deptName",
+        type: "input",
+        message: "What department would you like to add the item to?"
+      },
+      {
+        name: "price",
+        type: "input",
+        message: "What is the price of the item you would like to add?",
+        validate: function (value) {
+          if (isNaN(value) === false) {
+            return true;
+          }
+          return false;
+        }
+      },
+      {
+        name: "quantity",
+        type: "input",
+        message: "How many of the item do we have in stock?",
+        validate: function (value) {
+          if (isNaN(value) === false) {
+            return true;
+          }
+          return false;
+        }
+      }
+    ]).then(function (answer) {
+      connection.query("INSERT INTO products SET ?",
+        {
+          product_name: answer.itemName,
+          department_name: answer.deptName,
+          price: answer.price,
+          stock_quantity: answer.quantity
+        }, function (err) {
+          if (err) throw err;
+          console.log("item added succesfully");
+          showMenu();
+        }
+      )
+    })
 };
+
+function exit() {
+  connection.end();
+}
